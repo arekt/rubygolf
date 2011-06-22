dde1401d9c1746476e3c1744c75c0361e863f63c
arekt <arek@turlewicz.com>
2011-06-22 15:46:39 +0100

    Little bit more size reduction

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,12 +1,12 @@
 class Golf
-  def self.method_missing(method,*args)
-    method = /hole(\d)/.match(method.to_s)
-    if method_number = method[1]
+  def self.method_missing(m,*args)
+    m = /hole(\d)/.match(m.to_s)
+    if mn = m[1]
       a = pr()
       ak = sprintf(args[0].inspect)
-      ak.gsub!(/, /,',') unless method_number == '4'
-      ak.gsub!("\"","\'") if method_number == '9'
-      return eval a[method_number][ak]  
+      ak.gsub!(/, /,',') unless mn == '4'
+      ak.gsub!("\"","\'") if mn == '9'
+      return eval a[mn][ak]  
     end
   end
   private
@@ -16,14 +16,14 @@ class Golf
     File.open("spec/golf_spec.rb") do |f|
       while !f.eof? do
         l = f.readline
-        d << l if /Golf\.hole.* eql/.match(l)
-        if /Golf\.hole.* ==/.match(l)
+        d << l if /G.* eql/.match(l)
+        if /G.* ==/.match(l)
           d << l.chomp+f.readline
         end
       end
     end
     d.each do |l|
-       if /Golf\.hole(\d)\((.*)\).should (eql|==)[ ]+(.*)/.match(l) 
+       if /G.*e(\d)\((.*)\).should (eql|==)[ ]+(.*)/.match(l) 
         a[$1] ||={}
         a[$1][$2] = $4
        end 
